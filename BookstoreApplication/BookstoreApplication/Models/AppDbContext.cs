90295a553edc5500dc0e8d53345eb81f7c3bd58e
Uros <milinovicuros@gmail.com>
2025-09-28 02:59:27 +0200

    feat: v2 migrations

--- BookstoreApplication/BookstoreApplication/Models/AppDbContext.cs
+++ BookstoreApplication/BookstoreApplication/Models/AppDbContext.cs
@@ -20,13 +20,29 @@ namespace BookstoreApplication.Models
             modelBuilder.Entity<AuthorAward>()
                 .HasOne(aa => aa.Author)
                 .WithMany(a => a.AuthorAwards)
-                .HasForeignKey(aa => aa.AuthorId);
+                .HasForeignKey(aa => aa.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
+
 
             modelBuilder.Entity<AuthorAward>()
                 .HasOne(aa => aa.Award)
                 .WithMany(a => a.AuthorAwards)
-                .HasForeignKey(aa => aa.AwardId);
+                .HasForeignKey(aa => aa.AwardId)
+                .OnDelete(DeleteBehavior.Cascade);
 
+            modelBuilder.Entity<AuthorAward>()
+                .ToTable("AuthorAwardBridge");
+
+            modelBuilder.Entity<Author>()
+                .Property(a => a.DateOfBirth)
+                .HasColumnName("BirthDay");
+
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Publisher)
+                .WithMany()
+                .HasForeignKey(b => b.PublisherId)
+                .OnDelete(DeleteBehavior.Restrict);
+                
         }
 
     }
